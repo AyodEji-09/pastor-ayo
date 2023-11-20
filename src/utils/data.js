ef89e2013c4b44589f402b367cd977ef6cac1373
Abiodun-Samuel <abiodunsamyemi@gmail.com>
2023-11-20 05:30:13 +0100

    about page

--- src/utils/data.js
+++ src/utils/data.js
@@ -65,3 +65,49 @@ export const blogs = [
       "Examining how compelling storytelling can revolutionize marketing strategies and brand engagement.",
   },
 ];
+export const books = [
+  {
+    title: "THE EAGLE - THE WINNING STRATEGY",
+    description:
+      "Embracing the Skies: Soaring with the Eagle's Spirit is a transformative journey through the teachings of nature's majestic sentinel, the eagle. In this inspiring book, we delve into the symbolic wisdom of the eagle's flight and explore how its attributes can guide us toward personal growth, resilience, and success in our own lives. Through vivid narratives, relatable anecdotes, and thoughtful reflections, readers are invited to soar alongside the eagle, embracing its spirit as a source of inspiration and guidance. Whether you seek to overcome challenges, cultivate a stronger faith, or leave a lasting legacy, this book explores how the eagle's lessons can elevate your journey and empower you to reach new heights. Join us on this captivating expedition into the skies and discover the transformative power of Embracing the Skies: Soaring with the Eagle's Spirit.",
+    img_url: "https://m.media-amazon.com/images/I/61BFnWTR1bL._SY466_.jpg",
+    url: "https://www.amazon.com/EAGLE-WINNING-STRATEGY-Ayodeji-Anifowose/dp/B0CMJCP2NW/ref=monarch_sidesheet",
+    format: [
+      {
+        type: "Hardcover",
+        url: "https://www.amazon.com/EAGLE-WINNING-STRATEGY-Ayodeji-Anifowose/dp/B0CMJCP2NW/ref=monarch_sidesheet",
+        price: "$26.99",
+      },
+      {
+        type: "Paperback",
+        url: "https://www.amazon.com/EAGLE-WINNING-STRATEGY-Ayodeji-Anifowose/dp/B0CMK21LZK/ref=monarch_sidesheet",
+        price: "$18.99",
+      },
+    ],
+    dop: "November 2, 2023",
+    pages: "141 pages",
+    language: "English",
+  },
+  {
+    title: "GOD'S AGENDA FOR MAN: EMBRACING TRUE MANHOOD",
+    description:
+      "God's Agenda for Man: Embracing True Manhood invites readers on a transformative journey of understanding and embracing their unique calling as men. Throughout this book, we will explore the Biblical principles and timeless truths that define true manhood according to God's design. We will discover the meaning and essence of true manhood as the book treats essential concerns such as identity, purpose, relationships, and responsibility, while embodying the character of Christ. The book also challenges cultural norms that often distort and devalue true manhood, offering instead a vision of masculinity grounded in God's Word. I’m assured this book will not only be part of your library collection but will truly change your life",
+    img_url: "https://m.media-amazon.com/images/I/61MZgQP+AuL._SY466_.jpg",
+    url: "https://www.amazon.com/GODS-AGENDA-MAN-EMBRACING-MANHOOD/dp/B0CLVGJYKL/ref=monarch_sidesheet",
+    format: [
+      {
+        type: "Hardcover",
+        url: "https://www.amazon.com/GODS-AGENDA-MAN-EMBRACING-MANHOOD/dp/B0CLVGJYKL/ref=monarch_sidesheet",
+        price: "$30.00",
+      },
+      {
+        type: "Paperback",
+        url: "https://www.amazon.com/GODS-AGENDA-MAN-EMBRACING-MANHOOD/dp/B0CLVQK5Z7/ref=monarch_sidesheet",
+        price: "$20.00",
+      },
+    ],
+    dop: "November 4, 2023",
+    pages: "223 pages",
+    language: "English",
+  },
+];
