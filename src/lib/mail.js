d3a199de2e681d7a20eca6747f1cd9d2d0885ca8
Abiodun-Samuel <abiodunsamyemi@gmail.com>
2023-11-29 06:22:58 +0100

    testing resend mail api

--- src/lib/mail.js
+++ src/lib/mail.js
@@ -1,31 +1,27 @@
-// "use strict";
-// import nodemailer from "nodemailer";
+"use strict";
+import nodemailer from "nodemailer";
 
-// Require:
+export async function sendMail(message, subject, to) {
+  let transporter = nodemailer.createTransport({
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_PORT,
+    secure: true,
+    auth: {
+      user: process.env.MAIL_USERNAME,
+      pass: process.env.MAIL_PASSWORD,
+    },
+  });
 
-
-
-// export async function sendMail(message, subject, to) {
-//   let transporter = nodemailer.createTransport({
-//     host: process.env.MAIL_HOST,
-//     port: process.env.MAIL_PORT,
-//     secure: true,
-//     auth: {
-//       user: process.env.MAIL_USERNAME,
-//       pass: process.env.MAIL_PASSWORD,
-//     },
-//   });
-
-//   // send mail with defined transport object
-//   let info = await transporter.sendMail({
-//     from: {
-//       name: "Ayodeji Anifowose",
-//       address: process.env.MAIL_FROM, // sender address
-//     },
-//     to, // []
-//     subject, // Subject line
-//     html: message, // html body
-//     text: message,
-//   });
-//   return info;
-// }
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: {
+      name: "Ayodeji Anifowose",
+      address: process.env.MAIL_FROM, // sender address
+    },
+    to, // []
+    subject, // Subject line
+    html: message, // html body
+    text: message,
+  });
+  return info;
+}
