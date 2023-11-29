e859d77ba47b374b60bc1b337b13924593a6f4cd
Abiodun-Samuel <abiodunsamyemi@gmail.com>
2023-11-29 05:44:28 +0100

    testing email

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import { sendMail } from "@/lib/mail";
 import { contactMail } from "@/lib/email_templates/contact_mail";
 import { test } from "@/lib/email_templates/test";
+import { Resend } from "resend";
 
 export async function POST(request) {
   const body = await request.json();
@@ -16,9 +17,18 @@ export async function POST(request) {
     );
   }
   try {
-    let adminEmail = process.env.ADMIN_EMAIL;
+    const resend = new Resend(process.env.RESEND_API_KEY);
 
-    const response = sendMail(test(name, email, message), subject, adminEmail);
+    resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: "abiodunsamyemi@gmail.com",
+      subject: "Hello World",
+      html: "<p>Congrats on sending your <strong>first email</strong>!</p>",
+    });
+    // let adminEmail = process.env.ADMIN_EMAIL;
+
+    // const response = sendMail(test(name, email, message), subject, adminEmail);
+
     return NextResponse.json({ data: [], message: "success" }, { status: 200 });
   } catch (error) {
     return NextResponse.json(
